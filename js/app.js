3c64080ff21724716c4cbffe4435333da58c8db1
Michael Roncone <mjroncone@gmail.com>
2015-08-04 18:34:31 -0500

    remove: last js commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-var Model = {
+function model() {
 
 }
 
 
 // Following functions are from GoogleMaps' API documentation:
 //  https://developers.google.com/maps/documentation/javascript/tutorial
-function view() {
+function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng( 41.926411, -87.643326),
     zoom: 16
@@ -19,7 +19,7 @@ function startApp() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
-                  '&signed_in=true&callback=view';
+                  '&signed_in=true&callback=initialize';
   document.body.appendChild(script);
 }
 
