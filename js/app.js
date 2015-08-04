6ff962aa90a5442d696a35a2a00f5a4c39367720
Michael Roncone <mjroncone@gmail.com>
2015-08-04 18:22:24 -0500

    Feat: make googlemaps api async

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
-function initialize() {
+var Model = {
+
+}
+
+
+// Following functions are from GoogleMaps' API documentation:
+//  https://developers.google.com/maps/documentation/javascript/tutorial
+function view() {
   var mapOptions = {
-    center: { lat: 41.926411, lng: -87.643326},
+    center: new google.maps.LatLng( 41.926411, -87.643326),
     zoom: 16
   };
+
   var map = new google.maps.Map(document.getElementById('map-area'),
       mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function startApp() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
+                  '&signed_in=true&callback=view';
+  document.body.appendChild(script);
+}
+
+window.onload = startApp;
