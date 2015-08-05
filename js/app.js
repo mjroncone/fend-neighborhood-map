2d1a7cf56a5d5945900738d835c2c4ee0fb9ce69
Michael Roncone <mjroncone@gmail.com>
2015-08-05 18:03:38 -0500

    feat: simplify app engine, add model.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-function model() {
+function MapModel() {
+  var self = this;
 
-}
+  self.locations = [{
+
+  },{},{},{}];
+};
+
+
 
 
 // Following functions are from GoogleMaps' API documentation:
 //  https://developers.google.com/maps/documentation/javascript/tutorial
 function initialize() {
+  var lincolnParkChicago = {lat: 41.926411, lng: -87.643326};
+
   var mapOptions = {
-    center: new google.maps.LatLng( 41.926411, -87.643326),
-    zoom: 16
-  };
+   center : lincolnParkChicago,
+   zoom : 16
+  }
 
   var map = new google.maps.Map(document.getElementById('map-area'),
       mapOptions);
@@ -21,6 +29,7 @@ function startApp() {
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
                   '&signed_in=true&callback=initialize';
   document.body.appendChild(script);
+
 }
 
 window.onload = startApp;
