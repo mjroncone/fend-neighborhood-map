c2916e854218601a9d8c16d2b817a4638bc84278
Michael Roncone <mjroncone@gmail.com>
2015-08-05 20:37:30 -0500

    feat: add comments

--- js/app.js
+++ js/app.js
@@ -1,28 +1,22 @@
-function MapModel() {
-  var self = this;
-
-  self.locations = [{
-
-  },{},{},{}];
-};
-
-
-
-
 // Following functions are from GoogleMaps' API documentation:
 //  https://developers.google.com/maps/documentation/javascript/tutorial
 function initialize() {
+  // Creates LatLng object to feed into G.Maps API for map center position
   var lincolnParkChicago = {lat: 41.926411, lng: -87.643326};
 
+  // Sets the mapOptions that will be served in the API request
   var mapOptions = {
    center : lincolnParkChicago,
    zoom : 16
   }
 
+  // Creates a new G.Maps object and fixed it to the element with id 'map-area'
   var map = new google.maps.Map(document.getElementById('map-area'),
       mapOptions);
 }
 
+// Contains the script to actually make the API request, putting it here and
+// invoking it later allows us to load G.Maps asynchronously on window load.
 function startApp() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
