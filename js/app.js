b75f6908ea081ddf1e85b966c7bbb08dc16ca1ae
Michael Roncone <mjroncone@gmail.com>
2015-08-11 19:06:45 -0500

    feat: separate update and add markers, reorganize

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
+var map;
+
 // Following functions are from GoogleMaps' API documentation:
 //  https://developers.google.com/maps/documentation/javascript/tutorial
+
+// Contains the script to actually make the API request, putting it here and
+// invoking it later allows us to load G.Maps asynchronously on window load.
+function startApp() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
+                  '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+
+}
+
+window.onload = startApp;
+
 function initialize() {
   // Creates LatLng object to feed into G.Maps API for map center position
   var lincolnParkChicago = {lat: 41.926411, lng: -87.643326};
@@ -11,15 +27,30 @@ function initialize() {
   }
 
   // Creates a new G.Maps object and fixed it to the element with id 'map-area'
-  var map = new google.maps.Map(document.getElementById('map-area'),
-      mapOptions);
+  map = new google.maps.Map(document.getElementById('map-area'),
+    mapOptions);
 
+  updateMarkers();
+};
+
+function updateMarkers() {
+  var markerObject = {
+    starbucks: {
+      position: {lat: 41.928640 , lng: -87.642064},
+      title: 'Starbucks',
+      infoWindow: new google.maps.InfoWindow({
+        content: '<div>Starbucks!</div>'
+      }),
+    }
+  }
+  addMapsMarkers(markerObject);
+};
+
+function addMapsMarkers(markerObject) {
   var marker = new google.maps.Marker({
-    position: {lat: 41.928640 , lng: -87.642064},
-    title: 'Starbucks',
-    infowindow: new google.maps.InfoWindow({
-      content: '<div>Starbucks!</div>'
-    }),
+    position: markerObject.starbucks.position,
+    title: markerObject.starbucks.title,
+    infowindow: markerObject.starbucks.infoWindow,
     animation: google.maps.Animation.DROP,
     map: map
   });
@@ -36,17 +67,4 @@ function initialize() {
     marker.infowindow.open(map, marker);
     toggleBounce();
   });
-}
-
-// Contains the script to actually make the API request, putting it here and
-// invoking it later allows us to load G.Maps asynchronously on window load.
-function startApp() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
-                  '&signed_in=true&callback=initialize';
-  document.body.appendChild(script);
-
-}
-
-window.onload = startApp;
+};
