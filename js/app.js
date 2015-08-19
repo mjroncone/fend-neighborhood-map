9182ae0bdbf83c23dcaf93207735f4387d40159a
Michael Roncone <mjroncone@gmail.com>
2015-08-19 14:13:38 -0500

    feat: cosmetic fixes, plus larger venue radius

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ function startApp() {
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9HlnwXsPlOG1m3D6UVLNrW3O5LU7CHA' +
                   '&callback=initialize';
   document.body.appendChild(script);
-};
+}
 
 function initialize() {
   mapsErrorChecker();
@@ -38,41 +38,42 @@ function initialize() {
    center : mapTarget,
    zoom : ($(window).width() <= 400 ? 15 : 16),
    disableDefaultUI: true
-  }
+  };
 
   // Creates a new G.Maps object and fixed it to the element with id 'map-area'
   map = new google.maps.Map(document.getElementById('map-area'),
     mapOptions);
-};
+}
 
 /* closeVenues closes any open Google Maps info windows and stops the marker's animation
     whenever invoked */
 function closeVenues() {
   markerList().forEach(function(mrkr) {
-    if (mrkr.animation != null) {
+    if (mrkr.animation !== null) {
       mrkr.setAnimation(null);
       mrkr.infowindow.close(map, mrkr);
     }
   });
-};
+}
 
 /* fourSquareAjaxConstructor pieces together the Four Square API request URL and
     returns it to the invoking function. */
 function fourSquareAjaxConstructor(lat, lng) {
   var fourSqBaseRequest = 'https://api.foursquare.com/v2/venues/search?intent=browse';
   var target = '&ll=' + lat + ',' + lng;
-  var radius = '&radius=' + 500;
+  var radius = '&radius=' + 800;
+  var limit = '&limit=' + 100;
   var categoryIds = ['4bf58dd8d48988d1e0931735','4d4b7105d754a06374d81259'];
   var categories = '&categoryId=' + categoryIds.reduce(function(previousValue, currentValue){
-      return previousValue + ',' + currentValue});
+      return previousValue + ',' + currentValue;});
   var client_id = '&client_id=' + 'E2KGUFUAJOBTFYJLJT1NT50XKR3IDGYMFMOLG2G3GNXXUHB5';
   var client_secret= '&client_secret=' + '5ABPBLHRJNIRA5ROV0OMTNQGL3A5YMPNUPMY0J4UYOZONN5V';
   var version = '&v=' + '20150101';
 
-  var ajaxUrl = fourSqBaseRequest + target + radius + categories + client_id + client_secret + version;
+  var ajaxUrl = fourSqBaseRequest + target + radius + limit + categories + client_id + client_secret + version;
 
   return ajaxUrl;
-};
+}
 
 /* makeLocationsObject takes in the object from the API request and extracts only
     the information we need, placing it into locationsObject and feeding that
@@ -95,64 +96,67 @@ function makeLocationsObject(data) {
 
 function addMapsMarkers(locationsObject) {
   mapsErrorChecker();
-  // loops over the locations' object data and creates a new G.Maps marker for each
-  for (var business in locationsObject){
-    // Similar to declaring self = this, helps with loop readability.
-    var currLocation = locationsObject[business];
-    var currLocInfo = currLocation.location;
-    var currCategories = currLocation.categories[0];
-    // Marker construction starts here and goes until pushed onto the markerList
-    var marker = new google.maps.Marker({
+  // If present, loops over the locations' object data and creates a new G.Maps marker for each
+  if (!$.isEmptyObject(locationsObject)) {
+    for (var business in locationsObject){
+      // Similar to declaring self = this, helps with loop readability.
+      var currLocation = locationsObject[business];
+      var currLocInfo = currLocation.location;
+      var currCategories = currLocation.categories[0];
+      // Marker construction starts here and goes until pushed onto the markerList
+      var marker = new google.maps.Marker({
 
-      title: currLocation.name,
-      phone: currLocation.contact.formattedPhone,
-      address: currLocInfo.address + ', ' + currLocInfo.city + ' ' + currLocInfo.state,
-      position: {lat: currLocInfo.lat, lng: currLocInfo.lng},
-      menu: currLocation.hasMenu === true ? currLocation.menu.url : false,
-      category: 'Category: ' + currCategories.shortName,
-      keywords: currLocation.name + ' ' + currCategories.name + ' ' + currCategories.pluralName + ' ' + currCategories.shortName,
-      filter: function() {},
-      animation: google.maps.Animation.DROP,
-      map: null
-    });
+        title: currLocation.name,
+        phone: (currLocation.contact.formattedPhone === undefined ? 'Sorry, no phone number available.' : currLocation.contact.formattedPhone),
+        address: currLocInfo.address + ', ' + currLocInfo.city + ' ' + currLocInfo.state,
+        position: {lat: currLocInfo.lat, lng: currLocInfo.lng},
+        menu: currLocation.hasMenu === true ? currLocation.menu.url : false,
+        category: 'Category: ' + currCategories.shortName,
+        keywords: currLocation.name + ' ' + currCategories.name + ' ' + currCategories.pluralName + ' ' + currCategories.shortName,
+        filter: function() {},
+        animation: google.maps.Animation.DROP,
+        map: null
+      });
 
-    /* Contains the DOM construction of our infoWindows with data from each object.
-        content is also used through ko bindings for the list view*/
-    marker.infowindow = new google.maps.InfoWindow({
-      content: '<article class="infoWindow">' +
-                  '<h1 class="location-title">' +
-                    marker.title +
-                  '</h1>' +
-                  '<p>'+
-                    marker.phone + '<br>' +
-                    marker.address + '<br> ' +
-                    marker.category + '<br> ' +
-                    (marker.menu === false ? "Sorry, no menu available." : '<a href="' + marker.menu + '"target="blank" data-bind="click: openLink">Click to view the menu!</a>') +
-                  '</p>'+
+      /* Contains the DOM construction of our infoWindows with data from each object.
+          content is also used through ko bindings for the list view*/
+      marker.infowindow = new google.maps.InfoWindow({
+        content: '<article class="infoWindow">' +
+                    '<h1 class="location-title">' +
+                      marker.title +
+                    '</h1>' +
+                    '<p>'+
+                      marker.phone + '<br>' +
+                      marker.address + '<br> ' +
+                      marker.category + '<br> ' +
+                      (marker.menu === false ? "Sorry, no menu available." : '<a href="' + marker.menu + '"target="blank" data-bind="click: openLink">Click to view the menu!</a>') +
+                    '</p>'+
 
-                '</article>'
-    }),
+                  '</article>'
+      }),
 
-    // Starts or stops the bounce animation if a marker is clicked once or twice, respectively.
-    marker.toggleBounce = function() {
-      if (this.getAnimation() != null) {
-        this.setAnimation(null);
-      } else {
-        this.setAnimation(google.maps.Animation.BOUNCE);
-      }
-    };
+      // Starts or stops the bounce animation if a marker is clicked once or twice, respectively.
+      marker.toggleBounce = function() {
+        if (this.getAnimation() !== null) {
+          this.setAnimation(null);
+        } else {
+          this.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      };
 
-    // Closes any previously clicked markers and initiates the newly clicked marker.
-    google.maps.event.addListener(marker, 'click', function() {
-      closeVenues();
-      this.infowindow.open(map, this);
-      this.toggleBounce();
-    });
+      // Closes any previously clicked markers and initiates the newly clicked marker.
+      google.maps.event.addListener(marker, 'click', function() {
+        closeVenues();
+        this.infowindow.open(map, this);
+        this.toggleBounce();
+      });
 
-    // Each marker object is pushed onto our global array markerList for easy access anywhere.
-    markerList().push(marker);
+      // Each marker object is pushed onto our global array markerList for easy access anywhere.
+      markerList().push(marker);
+    }
+    console.log(markerList().length);
   }
-};
+}
 
 /* updateMarkers determines which venues should be displayed based on the current filter,
     sets their map, and returns them as an array to the invoking ViewModel for listView. */
@@ -189,6 +193,6 @@ function toggleSlide() {
   }
 
 
-};
+}
 
 document.getElementById("locations-button").addEventListener("click", toggleSlide);
